82e6b2fced895f5ca19ea4368ee87adfde4d0697
jamvar <varma.santosh@icloud.com>
2019-09-26 03:39:02 +0000

    add link and policy view

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -1,5 +1,6 @@
 class ComputersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_computer, only: [:link, :policy]
 
   def index
     @computers = Computer.all
@@ -7,5 +8,19 @@ class ComputersController < ApplicationController
 
   def credentials
     @credentials = ApiCredential.where(api_name: "5thcolumn").first
+  end
+
+  def link
+    @link = @computer.link
+  end
+
+  def policy
+    @policy = @computer.policy
+  end
+
+  private
+
+  def set_computer
+    @computer = Computer.find_by_id(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'computers#index'
   get '/api_credentials', to: 'computers#credentials', as: 'api_credentials'
+  get '/computers/:id/links', to: 'computers#link', as: 'computers_link'
+  get '/computers/:id/policy', to: 'computers#policy', as: 'computers_policy'
 end
